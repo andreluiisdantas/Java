01c19a02c9156b506fd1be1fa1eee2f899f0985f
andreluiisdantas <andreluissousaa10@gmail.com>
2025-05-23 11:23:44 -0300

    Repetição

--- Repeticao/src/Main.java
+++ Repeticao/src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
+        /*
         for(int i = 0; i< 10; i++){
+            System.out.println(i);
+        }
+         */
+
+        for(int i = 0; i <= 10; i++){
+            if(i % 2 == 0){
+                continue;
+            }
             System.out.println(i);
         }
     }
